ccaee13ca9979626e2d208ded4ef189ab4cc5107
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-01-03 19:14:29 -0300

    stripe checkout session

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
   resources :posts
+  post "checkout/create", to: "checkout#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
