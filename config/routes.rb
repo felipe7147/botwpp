43fa0e142e418c3f389680da04066eb234df84f4
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-01-03 16:10:10 -0300

    Product basic validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Metodo root joga a pagina e o metodo que vc deseja
+  root "products#index"
   resources :products
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
