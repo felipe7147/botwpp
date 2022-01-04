deb02758415e6ab0e5a3df79a1b71265739b4a0d
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-01-04 11:56:17 -0300

    Adicionando webhooks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :products
   resources :posts
   post "checkout/create", to: "checkout#create"
+  resources :webhooks, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
